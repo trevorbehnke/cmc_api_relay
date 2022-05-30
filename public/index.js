0c91440d4f8118967de4dad207b7da328845f28f
tab <trevorbehnke@gmail.com>
2022-05-29 19:14:53 -0700

    update function

--- public/index.js
+++ public/index.js
@@ -11,43 +11,59 @@ d3.json("http://localhost:5001/api").then(function (res) {
     .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-    .call(responsivefy) // this is all it takes to make the chart responsive
+    .call(responsivefy)
     .append("g")
     .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
   //  X axis
-  const xScale = d3
-    .scaleBand()
-    .padding(0.2)
-    .domain(data.map((d) => d.name))
-    .range([0, width]);
+  const xScale = d3.scaleBand().padding(0.2).range([0, width]);
 
-  svg
-    .append("g")
-    .attr("transform", `translate(0, ${height})`)
-    .call(d3.axisBottom(xScale))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+  const xAxis = svg.append("g").attr("transform", `translate(0,${height})`);
 
   // Y axis
-  const yScale = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, (d) => d.quote.USD.market_cap)])
-    .range([height, 0]);
+  const yScale = d3.scaleLinear().range([height, 0]);
 
-  svg.append("g").call(d3.axisLeft(yScale));
+  const yAxis = svg.append("g").attr("class", "myYaxis");
 
-  // Bars
-  svg
-    .selectAll("mybar")
-    .data(data)
-    .join("rect")
-    .attr("width", xScale.bandwidth())
-    .attr("x", (d) => xScale(d.name))
-    .attr("fill", "#00DAC5")
-    .attr("height", () => height - yScale(0))
-    .attr("y", (d) => yScale(0));
+  ////////
+  //  Update Function
+  ////////
+
+  function update(data) {
+    // Update the X axis
+    xScale.domain(data.map((d) => d.name));
+    xAxis
+      .call(d3.axisBottom(xScale))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
+
+    // Update the Y axis
+    yScale.domain([0, d3.max(data, (d) => d.quote.USD.market_cap)]);
+    yAxis.transition().duration(1000).call(d3.axisLeft(yScale));
+
+    var u = svg.selectAll("rect").data(data);
+
+    u.join("rect")
+      .transition()
+      .attr("x", (d) => xScale(d.name))
+      .attr("y", (d) => yScale(0))
+      .attr("width", xScale.bandwidth())
+      .attr("height", () => height - yScale(0))
+      .attr("fill", "#69b3a2")
+      .transition()
+      .attr("y", (d) => yScale(d.quote.USD.market_cap))
+      .attr("height", (d) => height - yScale(d.quote.USD.market_cap))
+      .delay((d, i) => {
+        return i * 25;
+      });
+  }
+
+  update(data);
+
+  ////////
+  //    Responsive Function
+  ////////
 
   // https://codepen.io/bclinkinbeard/pen/gGPvrz
   function responsivefy(svg) {
@@ -70,7 +86,10 @@ d3.json("http://localhost:5001/api").then(function (res) {
     }
   }
 
-  // Color Buttons
+  ////////
+  //    Color Buttons
+  ////////
+
   // green button
   const green_btn = document.getElementById("green_btn");
   green_btn.addEventListener("click", () => changeColor("#00DAC5"));
@@ -82,14 +101,4 @@ d3.json("http://localhost:5001/api").then(function (res) {
   function changeColor(color) {
     d3.selectAll("rect").transition().style("fill", color);
   }
-
-  // Animation
-  svg
-    .selectAll("rect")
-    .transition()
-    .attr("y", (d) => yScale(d.quote.USD.market_cap))
-    .attr("height", (d) => height - yScale(d.quote.USD.market_cap))
-    .delay((d, i) => {
-      return i * 25;
-    });
 });
